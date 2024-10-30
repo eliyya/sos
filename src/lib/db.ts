498658b51ce6d9d09f146222a6be2958dd57849e
LizjmH <Lizethjimenez399@outlook.es>
2024-10-30 11:31:31 -0600

    init repo

---
+++ src/lib/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client'
+
+declare const globalThis: {
+    prismaGlobal: PrismaClient
+} & typeof global
+
+const db = globalThis.prismaGlobal ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db
+
+export { db }
