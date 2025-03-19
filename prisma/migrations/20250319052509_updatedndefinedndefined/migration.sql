b75f8fc26f651f536660a51913318b4f81243de1
Elí Maciel <tecno_eli@hotmail.com>
2025-03-18 23:25:33 -0600

    migration

---
+++ prisma/migrations/20250319052509_updatedndefinedndefined/migration.sql
@@ -0,0 +1,285 @@
+-- CreateEnum
+CREATE TYPE "LABORATORY_TYPE" AS ENUM ('COMPUTER_CENTER', 'LABORATORY');
+
+-- CreateEnum
+CREATE TYPE "MACHINE_STATUS" AS ENUM ('AVAILABLE', 'MAINTENANCE');
+
+-- CreateEnum
+CREATE TYPE "STATUS" AS ENUM ('ACTIVE', 'ARCHIVED', 'DELETED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" BIGINT NOT NULL DEFAULT 2,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+    "status" "STATUS" NOT NULL DEFAULT 'ACTIVE',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auths" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "totp" TEXT,
+
+    CONSTRAINT "auths_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subjects" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "practice_hours" INTEGER NOT NULL,
+    "theory_hours" INTEGER NOT NULL,
+
+    CONSTRAINT "subjects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "ip" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "browser" TEXT NOT NULL,
+    "os" TEXT NOT NULL,
+    "device" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "classes" (
+    "id" TEXT NOT NULL,
+    "subject_id" TEXT NOT NULL,
+    "teacher_id" TEXT NOT NULL,
+    "career_id" TEXT NOT NULL,
+    "status" "STATUS" NOT NULL DEFAULT 'ACTIVE',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "laboratories" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "open_hour" INTEGER NOT NULL,
+    "close_hour" INTEGER NOT NULL,
+    "type" "LABORATORY_TYPE" NOT NULL DEFAULT 'LABORATORY',
+    "status" "STATUS" NOT NULL DEFAULT 'ACTIVE',
+
+    CONSTRAINT "laboratories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "practices" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "topic" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "students" INTEGER,
+    "registered_by" TEXT NOT NULL,
+    "laboratory_id" TEXT NOT NULL,
+    "observations" TEXT,
+    "class_id" TEXT NOT NULL,
+    "status" "STATUS" NOT NULL DEFAULT 'ACTIVE',
+
+    CONSTRAINT "practices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "software" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "software_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "software_practices" (
+    "software_id" TEXT NOT NULL,
+    "practice_id" TEXT NOT NULL,
+
+    CONSTRAINT "software_practices_pkey" PRIMARY KEY ("software_id","practice_id")
+);
+
+-- CreateTable
+CREATE TABLE "careers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "careers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "students" (
+    "nc" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "semester" INTEGER NOT NULL DEFAULT 1,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "career_id" TEXT NOT NULL,
+
+    CONSTRAINT "students_pkey" PRIMARY KEY ("nc")
+);
+
+-- CreateTable
+CREATE TABLE "visits" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "exit_at" TIMESTAMP(3),
+    "student_nc" TEXT NOT NULL,
+    "laboratory_id" TEXT NOT NULL,
+    "machine_id" TEXT,
+
+    CONSTRAINT "visits_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "machines" (
+    "id" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "status" "MACHINE_STATUS" NOT NULL DEFAULT 'AVAILABLE',
+    "processor" TEXT NOT NULL,
+    "ram" TEXT NOT NULL,
+    "storage" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "laboratory_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "serie" TEXT,
+
+    CONSTRAINT "machines_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "software_machines" (
+    "software_id" TEXT NOT NULL,
+    "machine_id" TEXT NOT NULL,
+
+    CONSTRAINT "software_machines_pkey" PRIMARY KEY ("software_id","machine_id")
+);
+
+-- CreateTable
+CREATE TABLE "issue" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "comment" TEXT NOT NULL,
+    "machine_id" TEXT NOT NULL,
+    "closed_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "issue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "comment" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "issue_id" TEXT NOT NULL,
+
+    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "student_classes" (
+    "student_nc" TEXT NOT NULL,
+    "class_id" TEXT NOT NULL,
+
+    CONSTRAINT "student_classes_pkey" PRIMARY KEY ("student_nc","class_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_secret_key" ON "sessions"("secret");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "laboratories_name_key" ON "laboratories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "careers_name_key" ON "careers"("name");
+
+-- AddForeignKey
+ALTER TABLE "auths" ADD CONSTRAINT "auths_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "classes" ADD CONSTRAINT "classes_subject_id_fkey" FOREIGN KEY ("subject_id") REFERENCES "subjects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "classes" ADD CONSTRAINT "classes_teacher_id_fkey" FOREIGN KEY ("teacher_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "classes" ADD CONSTRAINT "classes_career_id_fkey" FOREIGN KEY ("career_id") REFERENCES "careers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "practices" ADD CONSTRAINT "practices_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "practices" ADD CONSTRAINT "practices_registered_by_fkey" FOREIGN KEY ("registered_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "practices" ADD CONSTRAINT "practices_laboratory_id_fkey" FOREIGN KEY ("laboratory_id") REFERENCES "laboratories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "software_practices" ADD CONSTRAINT "software_practices_software_id_fkey" FOREIGN KEY ("software_id") REFERENCES "software"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "software_practices" ADD CONSTRAINT "software_practices_practice_id_fkey" FOREIGN KEY ("practice_id") REFERENCES "practices"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_career_id_fkey" FOREIGN KEY ("career_id") REFERENCES "careers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "visits" ADD CONSTRAINT "visits_student_nc_fkey" FOREIGN KEY ("student_nc") REFERENCES "students"("nc") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "visits" ADD CONSTRAINT "visits_laboratory_id_fkey" FOREIGN KEY ("laboratory_id") REFERENCES "laboratories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "visits" ADD CONSTRAINT "visits_machine_id_fkey" FOREIGN KEY ("machine_id") REFERENCES "machines"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "machines" ADD CONSTRAINT "machines_laboratory_id_fkey" FOREIGN KEY ("laboratory_id") REFERENCES "laboratories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "software_machines" ADD CONSTRAINT "software_machines_software_id_fkey" FOREIGN KEY ("software_id") REFERENCES "software"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "software_machines" ADD CONSTRAINT "software_machines_machine_id_fkey" FOREIGN KEY ("machine_id") REFERENCES "machines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "issue" ADD CONSTRAINT "issue_machine_id_fkey" FOREIGN KEY ("machine_id") REFERENCES "machines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "issue" ADD CONSTRAINT "issue_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_issue_id_fkey" FOREIGN KEY ("issue_id") REFERENCES "issue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_classes" ADD CONSTRAINT "student_classes_student_nc_fkey" FOREIGN KEY ("student_nc") REFERENCES "students"("nc") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_classes" ADD CONSTRAINT "student_classes_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "classes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
