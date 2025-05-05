865d36255e307638b20dd47920f8c2cc8ce60b25
LizjmH <Lizethjimenez399@outlook.es>
2025-05-04 19:59:38 -0600

    componets

---
+++ src/app/dashboard/management/software/components/ArchiveDialog.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { archiveStudent } from '@/actions/students'
+import { Button } from '@/components/Button'
+import { Dialog, DialogContent, DialogTitle } from '@/components/Dialog'
+import {
+    openArchiveAtom,
+    entityToEditAtom,
+    updateAtom,
+} from '@/global/managment-students'
+import { DialogDescription } from '@radix-ui/react-dialog'
+import { useAtom, useAtomValue, useSetAtom } from 'jotai'
+import { Archive, Ban } from 'lucide-react'
+import { useState, useTransition } from 'react'
+
+export function ArchiveDialog() {
+    const [open, setOpen] = useAtom(openArchiveAtom)
+    const [inTransition, startTransition] = useTransition()
+    const entity = useAtomValue(entityToEditAtom)
+    const [message, setMessage] = useState('')
+    const updateUsersTable = useSetAtom(updateAtom)
+
+    if (!entity) return null
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogTitle>
+                    <span className='text-3xl'>Archivar Estudiante</span>
+                </DialogTitle>
+                <DialogDescription>
+                    ¿Está seguro de archivar a {entity.firstname}{' '}
+                    {entity.lastname}?
+                </DialogDescription>
+                <form
+                    action={data => {
+                        startTransition(async () => {
+                            const { error } = await archiveStudent(data)
+                            if (error) {
+                                setMessage(error)
+                                setTimeout(() => setMessage('error'), 5_000)
+                            } else {
+                                setTimeout(
+                                    () => updateUsersTable(Symbol()),
+                                    1_000,
+                                )
+                                setOpen(false)
+                            }
+                        })
+                    }}
+                    className='flex w-full max-w-md flex-col justify-center gap-6'
+                >
+                    {message && (
+                        <span className='animate-slide-in mt-1 block rounded-lg bg-red-100 px-3 py-1 text-sm text-red-600 shadow-md'>
+                            {message}
+                        </span>
+                    )}
+                    <input type='hidden' value={entity.nc} name='nc' />
+                    <div className='flex flex-row gap-2 *:flex-1'>
+                        <Button
+                            disabled={inTransition}
+                            onClick={e => {
+                                e.preventDefault()
+                                setOpen(false)
+                            }}
+                        >
+                            <Ban className='mr-2 h-5 w-5' />
+                            Cancelar
+                        </Button>
+                        <Button
+                            type='submit'
+                            variant={'destructive'}
+                            disabled={inTransition}
+                        >
+                            <Archive className='mr-2 h-5 w-5' />
+                            Archivar
+                        </Button>
+                    </div>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
---
+++ src/app/dashboard/management/software/components/EntityTable.tsx
@@ -0,0 +1,144 @@
+'use client'
+
+import { Button } from '@/components/Button'
+import {
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
+    Table,
+} from '@/components/Table'
+import { STATUS, Student } from '@prisma/client'
+import { Archive, ArchiveRestore, Pencil, Trash2 } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { useAtomValue, useSetAtom } from 'jotai'
+import {
+    editDialogAtom,
+    openArchiveAtom,
+    openDeleteAtom,
+    openUnarchiveAtom,
+    showArchivedAtom,
+    entityToEditAtom,
+    updateAtom,
+} from '@/global/managment-students'
+import { getStudentsWithCareer } from '@/actions/students'
+import { EditDialog } from './EditDialog'
+import { ArchiveDialog } from './ArchiveDialog'
+import { UnarchiveDialog } from './UnarchiveDialog'
+import { DeleteDialog } from './DeleteDialog'
+
+export function EntityTable() {
+    const [entity, setEntity] = useState<
+        Awaited<ReturnType<typeof getStudentsWithCareer>>
+    >([])
+    const update = useAtomValue(updateAtom)
+    const archived = useAtomValue(showArchivedAtom)
+
+    useEffect(() => {
+        getStudentsWithCareer().then(setEntity)
+    }, [update])
+
+    return (
+        <>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Nombre</TableHead>
+                        <TableHead>Apellido</TableHead>
+                        <TableHead>Carrera</TableHead>
+                        <TableHead>Semestre</TableHead>
+                        <TableHead>Options</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {entity
+                        .filter(
+                            u =>
+                                (u.status === STATUS.ACTIVE && !archived) ||
+                                (u.status === STATUS.ARCHIVED && archived),
+                        )
+                        .map(entity => (
+                            <TableRow key={entity.nc}>
+                                <TableCell>{entity.firstname}</TableCell>
+                                <TableCell>{entity.lastname}</TableCell>
+                                <TableCell>
+                                    {entity.career.alias ?? entity.career.name}
+                                </TableCell>
+                                <TableCell>{entity.semester}</TableCell>
+                                <TableCell className='flex gap-0.5'>
+                                    <Buttons entity={entity} />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                </TableBody>
+            </Table>
+            <EditDialog />
+            <ArchiveDialog />
+            <UnarchiveDialog />
+            <DeleteDialog />
+        </>
+    )
+}
+
+interface ButtonsProps {
+    entity: Student
+}
+function Buttons({ entity }: ButtonsProps) {
+    const openEditDialog = useSetAtom(editDialogAtom)
+    const setSubjectSelected = useSetAtom(entityToEditAtom)
+    const setArchiveDialog = useSetAtom(openArchiveAtom)
+    const openUnarchiveDialog = useSetAtom(openUnarchiveAtom)
+    const openDeleteDialog = useSetAtom(openDeleteAtom)
+    if (entity.status === STATUS.ACTIVE)
+        return (
+            <>
+                {/* Editar */}
+                <Button
+                    size='icon'
+                    onClick={() => {
+                        openEditDialog(true)
+                        setSubjectSelected(entity)
+                    }}
+                >
+                    <Pencil className='text-xs' />
+                </Button>
+                {/* Archivar */}
+                <Button
+                    size='icon'
+                    onClick={() => {
+                        setSubjectSelected(entity)
+                        setArchiveDialog(true)
+                    }}
+                >
+                    <Archive className='w-xs text-xs' />
+                </Button>
+            </>
+        )
+    if (entity.status === STATUS.ARCHIVED)
+        return (
+            <>
+                {/* Unarchive */}
+                <Button
+                    size='icon'
+                    onClick={() => {
+                        setSubjectSelected(entity)
+                        openUnarchiveDialog(true)
+                    }}
+                >
+                    <ArchiveRestore className='w-xs text-xs' />
+                </Button>
+                {/* Delete */}
+                <Button
+                    size='icon'
+                    onClick={() => {
+                        setSubjectSelected(entity)
+                        openDeleteDialog(true)
+                    }}
+                >
+                    <Trash2 className='w-xs text-xs' />
+                </Button>
+            </>
+        )
+    return <></>
+}
