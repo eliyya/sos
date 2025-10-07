20436d54bae04f140acc4f80c3ffae376d505332
Elí Maciel <tecno_eli@hotmail.com>
2025-10-06 21:03:30 -0600

    edit laboratories migrated

--- src/app/dashboard/management/laboratories/components/EditDialog.tsx
+++ src/app/dashboard/management/laboratories/components/EditDialog.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import { LABORATORY_TYPE } from '@/prisma/generated/enums'
-import { useAtom, useAtomValue, useSetAtom } from 'jotai'
-import { ClockIcon, MicroscopeIcon, Save, SquarePenIcon } from 'lucide-react'
-import { useState, useTransition } from 'react'
-import { editLaboratory } from '@/actions/laboratory'
+import { useAtom, useAtomValue } from 'jotai'
+import {
+    ClockIcon,
+    MicroscopeIcon,
+    SaveIcon,
+    SquarePenIcon,
+} from 'lucide-react'
+import { useCallback, useState, useTransition } from 'react'
 import { Button } from '@/components/Button'
 import {
     Dialog,
@@ -16,12 +20,14 @@ import {
 import { MessageError } from '@/components/Error'
 import { RetornableCompletInput } from '@/components/Inputs'
 import { RetornableCompletSelect } from '@/components/Select'
-import {
-    editDialogAtom,
-    entityToEditAtom,
-    updateAtom,
-} from '@/global/management-laboratory'
 import { secondsToTime } from '@/lib/utils'
+import {
+    openDialogAtom,
+    selectedLaboratoryAtom,
+} from '@/global/laboratories.globals'
+import { editLaboratory } from '@/actions/laboratories.actions'
+import { useLaboratories } from '@/hooks/laboratories.hoohs'
+import { useRouter } from 'next/navigation'
 
 const labTypeLabel = {
     [LABORATORY_TYPE.LABORATORY]: 'Laboratorio',
@@ -29,16 +35,72 @@ const labTypeLabel = {
 }
 
 export function EditDialog() {
-    const [open, setOpen] = useAtom(editDialogAtom)
+    const [dialogOpened, openDialog] = useAtom(openDialogAtom)
     const [inTransition, startTransition] = useTransition()
-    const old = useAtomValue(entityToEditAtom)
+    const old = useAtomValue(selectedLaboratoryAtom)
     const [message, setMessage] = useState('')
-    const updateUsersTable = useSetAtom(updateAtom)
+    const { setLaboratories, refetchLaboratories } = useLaboratories()
+    const router = useRouter()
+
+    const onAction = useCallback(
+        ({
+            name,
+            close_hour,
+            open_hour,
+            type,
+        }: {
+            name?: string
+            close_hour?: number
+            open_hour?: number
+            type?: LABORATORY_TYPE
+        }) => {
+            if (!old) return
+            startTransition(async () => {
+                const response = await editLaboratory({
+                    name,
+                    close_hour,
+                    open_hour,
+                    type,
+                })
+                if (response.status === 'success') {
+                    setLaboratories(labs =>
+                        labs.map(lab =>
+                            lab.id !== old.id ? lab : response.laboratory,
+                        ),
+                    )
+                    openDialog(null)
+                } else {
+                    if (response.type === 'already-exists') {
+                        setMessage('El laboratorio ya existe')
+                    } else if (response.type === 'invalid-input') {
+                        setMessage(response.message)
+                    } else if (response.type === 'not-found') {
+                        await refetchLaboratories()
+                        openDialog(null)
+                    } else if (response.type === 'permission') {
+                        setMessage(
+                            'No tienes permiso para editar este laboratorio',
+                        )
+                    } else if (response.type === 'unauthorized') {
+                        router.replace('/login')
+                    } else if (response.type === 'unexpected') {
+                        setMessage('Ha ocurrido un error, intente más tarde')
+                    }
+                }
+            })
+        },
+        [old, openDialog, setLaboratories, router, refetchLaboratories],
+    )
 
     if (!old) return null
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
+        <Dialog
+            open={dialogOpened === 'EDIT'}
+            onOpenChange={state => {
+                if (!state) openDialog(null)
+            }}
+        >
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Editar Laboratorio</DialogTitle>
@@ -48,17 +110,11 @@ export function EditDialog() {
                 </DialogHeader>
                 <form
                     action={data => {
-                        startTransition(async () => {
-                            const { error } = await editLaboratory(data)
-                            if (error) setMessage(error)
-                            else {
-                                setTimeout(
-                                    () => updateUsersTable(Symbol()),
-                                    500,
-                                )
-                                setOpen(false)
-                            }
-                        })
+                        const name = data.get('name') as string
+                        const close_hour = Number(data.get('close_hour'))
+                        const open_hour = Number(data.get('open_hour'))
+                        const type = data.get('type') as LABORATORY_TYPE
+                        onAction({ name, close_hour, open_hour, type })
                     }}
                     className='flex w-full max-w-md flex-col justify-center gap-6'
                 >
@@ -110,7 +166,7 @@ export function EditDialog() {
                         icon={MicroscopeIcon}
                     />
                     <Button type='submit' disabled={inTransition}>
-                        <Save className='mr-2 h-5 w-5' />
+                        <SaveIcon className='mr-2 h-5 w-5' />
                         Save
                     </Button>
                 </form>
