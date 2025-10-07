33228324b0fc685d8ef86668d76f4620f2288735
Elí Maciel <tecno_eli@hotmail.com>
2025-10-06 22:40:27 -0600

    management button rendered conditionaly

--- src/app/dashboard/components/management-link.tsx
+++ src/app/dashboard/components/management-link.tsx
@@ -1,4 +1,9 @@
+import {
+    PERMISSIONS_FLAGS,
+    PermissionsBitField,
+} from '@/bitfields/PermissionsBitField'
 import { managementRouteSelected } from '@/global/routes.globals'
+import { authClient } from '@/lib/auth-client'
 import { cn } from '@/lib/utils'
 import { useAtom } from 'jotai'
 import { BeakerIcon } from 'lucide-react'
@@ -9,10 +14,30 @@ import { useEffect } from 'react'
 export function ManagementLink() {
     const pathname = usePathname().replace(/\/$/, '')
     const [route, setRoute] = useAtom(managementRouteSelected)
+    const { data: session } = authClient.useSession()
+    const permissions =
+        session ?
+            new PermissionsBitField(session.user.permissions)
+        :   new PermissionsBitField()
 
     useEffect(() => {
         if (pathname.includes('/dashboard/management')) setRoute(pathname)
     }, [pathname, setRoute])
+
+    if (
+        !permissions.any(
+            PERMISSIONS_FLAGS.MANAGE_LABS |
+                PERMISSIONS_FLAGS.MANAGE_USERS |
+                PERMISSIONS_FLAGS.MANAGE_ROLES |
+                PERMISSIONS_FLAGS.MANAGE_SUBJECTS |
+                PERMISSIONS_FLAGS.MANAGE_CAREERS |
+                PERMISSIONS_FLAGS.MANAGE_CLASSES |
+                PERMISSIONS_FLAGS.MANAGE_STUDENTS |
+                PERMISSIONS_FLAGS.MANAGE_MACHINES |
+                PERMISSIONS_FLAGS.MANAGE_SOFTWARE,
+        )
+    )
+        return null
 
     return (
         <Link
