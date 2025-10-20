ff8853ccc0e43b7bff39339bd4d2588125026e97
Elí Maciel <tecno_eli@hotmail.com>
2025-10-19 22:25:25 -0600

    fix i18n initializationn

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,24 @@ import { withTypeCSSModule } from '@eliyya/typed-css-modules'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 export default withTypeCSSModule(
-    createNextIntlPlugin('./src/i18n.ts')(
-        withTypeRoute({
-            allowedDevOrigins: ['dev.eliyya.dev'],
-            // la configuracion de nextjs
-            webpack: config => {
-                config.ignoreWarnings = [
-                    {
-                        message:
-                            /Critical dependency: the request of a dependency is an expression/,
-                    },
-                ]
-                return config
+    createNextIntlPlugin()(
+        withTypeRoute(
+            {
+                allowedDevOrigins: ['dev.eliyya.dev'],
+                // la configuracion de nextjs
+                webpack: config => {
+                    config.ignoreWarnings = [
+                        {
+                            message:
+                                /Critical dependency: the request of a dependency is an expression/,
+                        },
+                    ]
+                    return config
+                },
             },
-        },
-        {
-            definedParams: {},
-        }),
+            {
+                definedParams: {},
+            },
+        ),
     ),
 )
