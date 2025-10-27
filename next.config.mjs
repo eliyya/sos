5119b1f9d9cbcafdc4ace32f67cc55aa846ba41a
Elí Maciel <tecno_eli@hotmail.com>
2025-10-26 19:56:07 -0600

    clear next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,29 +2,15 @@ import { withTypeRoute } from '@eliyya/type-routes/next'
 import { withTypeCSSModule } from '@eliyya/typed-css-modules'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-export default withTypeCSSModule(
-    createNextIntlPlugin()(
-        withTypeRoute(
-            {
-                // la configuracion de nextjs
-                allowedDevOrigins: ['dev.eliyya.dev'],
-                // cacheComponents: true,
-                reactCompiler: true,
-                webpack: config => {
-                    config.ignoreWarnings = [
-                        {
-                            message:
-                                /Critical dependency: the request of a dependency is an expression/,
-                        },
-                    ]
-                    return config
-                },
-            },
-            {
-                definedParams: {
-                    locale: ['es'],
-                },
-            },
-        ),
-    ),
-)
+let config = withTypeCSSModule({
+    allowedDevOrigins: ['dev.eliyya.dev'],
+    reactCompiler: true,
+})
+config = createNextIntlPlugin()(config)
+config = withTypeRoute(config, {
+    definedParams: {
+        locale: ['es'],
+    },
+})
+
+export default config
